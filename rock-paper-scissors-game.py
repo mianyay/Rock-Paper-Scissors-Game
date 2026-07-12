2e5ae68b89452dc00e94efce7064b0a4a25eb151
mianyay <mianguye00@gmail.com>
2026-07-12 14:24:30 +1000

    slow print to make the game play nicer

--- rock-paper-scissors-game.py
+++ rock-paper-scissors-game.py
@@ -1,6 +1,19 @@
 import time
 import random
 
+player_score = 0
+computer_score = 0
+ties = 0
+
+def slow_print(text):
+    for char in text:
+        print(char, end ="", flush = True)
+        time.sleep(0.04)
+
+greeting = "Welcome to the rock, paper, scissors game! \n"
+time.sleep(1)
+slow_print(greeting)
+
 while True: # anything indented here will play while it is True
     options = """
 Please select a choice underneath by the number:
@@ -8,7 +21,7 @@ Please select a choice underneath by the number:
 2 - Paper
 3 - Scissors
 4 - Quit Game \n"""
-    print(options)
+    slow_print(options)
     try:
         choose = int(input("Enter your choice: "))
     except ValueError:
@@ -34,7 +47,7 @@ Please select a choice underneath by the number:
 
     time.sleep(1)
 
-    print("Computer turn... \n")
+    slow_print("Computer turn... \n")
     time.sleep(0.5)
     computer_roll = random.randint(1, 3)
     if computer_roll == 1:
@@ -55,14 +68,19 @@ Please select a choice underneath by the number:
     time.sleep(1)
 
     if user_choice == computer_choice:
-        print('\x1b[34m' + "It's a tie! \n" + '\033[39m')
-
+        slow_print('\x1b[34m' + "It's a tie! \n" + '\033[39m')
+        ties += 1
     elif user_choice == "Rock" and computer_choice == "Scissors" or user_choice == "Scissors" and computer_choice == "Paper" or user_choice == "Paper" and computer_choice == "Rock":
-        print('\033[32m' + f"""User wins, {user_choice} beat {computer_choice}!
+        slow_print('\033[32m' + f"""User wins, {user_choice} beat {computer_choice}!
 == User wins == \n""" + '\033[39m')
-
+        player_score += 1
     else:
-        print('\033[31m' + f"""Computer wins, {computer_choice} beat {user_choice}! Haha
+        slow_print('\033[31m' + f"""Computer wins, {computer_choice} beat {user_choice}! Haha
 == User lost == \n""" + '\033[39m')
+        computer_score += 1
+    
+    time.sleep(1)
+    slow_print('\033[1m' + f"Score: Player {player_score} | Computer {computer_score} | Ties {ties} \n" + '\033[0m')
+    time.sleep(1)
 
-    print("-" * 50)
+    slow_print("-" * 50)
